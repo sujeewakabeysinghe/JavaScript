87388d1fb4f604667a7c136858c4ad27cab5fad2
Sujeewa K. Abeysinghe <sujeewakabeysinghe@gmail.com>
2023-07-02 23:23:36 +0530

    index-2

---
+++ javascript/index-2.js
@@ -0,0 +1,11 @@
+let age = 21;
+let name = 'sujeewa';
+let married = false;
+age = age+1;
+name = name +1;
+
+console.log(age);
+console.log('I\'m not',name, 'bro');
+console.log(married);
+
+document.getElementById('js').innerHTML = 'I\'m not '+name+' bro';

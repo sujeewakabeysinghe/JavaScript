902719d8e89c73c89c31bb09897e51cd0542129a
Sujeewa K. Abeysinghe <sujeewakabeysinghe@gmail.com>
2023-07-03 00:03:15 +0530

    index-5

---
+++ javascript/index-5.js
@@ -0,0 +1,24 @@
+let age = window.prompt('how old are you?');
+
+console.log(typeof age);
+age = Number(age);
+console.log(typeof age);
+age += 1;
+
+console.log(age);
+
+let x;
+let y;
+let z;
+
+x1 = Number('6.63');
+x2 = Number('hey');
+y = String(5.32);
+z1 = Boolean('');
+z2 = Boolean('hey');
+
+console.log(x1, typeof x2);
+console.log(x2, typeof x2);
+console.log(y, typeof y);
+console.log(z1, typeof z1);
+console.log(z2, typeof z2);

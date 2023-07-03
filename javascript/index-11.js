43805fc3b4a68dfd663399a4c3b33adeb76c99c1
Sujeewa K. Abeysinghe <sujeewakabeysinghe@gmail.com>
2023-07-03 10:03:37 +0530

    index-11

---
+++ javascript/index-11.js
@@ -0,0 +1,12 @@
+let a = '   Bro code    ';
+
+console.log(a);
+console.log(a.length);
+console.log(a.charAt(3));
+console.log(a.indexOf('o'));
+console.log(a.lastIndexOf('o'));
+console.log(a.trim())
+console.log(a.toUpperCase());
+console.log(a.toLowerCase());
+console.log(a.replace('o', ' '));
+console.log(a.replaceAll('o', ' '));

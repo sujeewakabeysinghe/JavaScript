4ef158382e446140af831f4b4ae555a510c75058
Sujeewa K. Abeysinghe <sujeewakabeysinghe@gmail.com>
2023-07-03 10:36:03 +0530

    index-14

---
+++ javascript/index-14.js
@@ -0,0 +1,12 @@
+let a = Math.random()
+
+if(a > 0.7) {
+    console.log(a);
+    console.log('true');
+} else if(a <= 0.6 && a >= 0.3) {
+    console.log(a);
+    console.log('true-false');
+} else {
+    console.log(a);
+    console.log('false');
+}

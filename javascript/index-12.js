f2c2c868484f45e22d38d8f47b56cb380392be99
Sujeewa K. Abeysinghe <sujeewakabeysinghe@gmail.com>
2023-07-03 10:10:02 +0530

    index-12

---
+++ javascript/index-12.js
@@ -0,0 +1,7 @@
+let a = 'Bro code';
+
+console.log(a.slice(0, 3));
+console.log(a.slice(4));
+console.log(a.slice(0, a.indexOf(' ')))
+console.log(a.slice(a.indexOf(' ') + 1))
+console.log(a.slice(a.lastIndexOf('o') - 1))

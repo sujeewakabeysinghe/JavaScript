d8a37ebea64f88a9bce5b48e2983b8f82531ebc2
Sujeewa K. Abeysinghe <sujeewakabeysinghe@gmail.com>
2023-07-16 02:00:47 +0530

    index-49

---
+++ javascript/index-49.js
@@ -0,0 +1,13 @@
+let car = {
+    color : 'Ash',
+    doors : 2,
+    drive : function() {
+        console.log('Drive');
+    },
+    stop : function() {
+        console.log('Stop');
+    }
+}
+
+console.log(car.color);
+car.drive();

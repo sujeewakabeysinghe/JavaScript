6e8e3829815813a69039c6ae8ae9c4712f023c14
Sujeewa K. Abeysinghe <sujeewakabeysinghe@gmail.com>
2023-07-23 18:58:20 +0530

    index-70

---
+++ javascript/index-70-module-util.js
@@ -0,0 +1,5 @@
+export const CONST = 2.25;
+
+export function myFunction(a, b) {
+    return a * b;
+}
---
+++ javascript/index-70.js
@@ -0,0 +1,8 @@
+import { CONST, myFunction } from './index-70-module-util.js';
+import * as myModule from './index-70-module-util.js';
+
+console.log(CONST);
+console.log(myFunction(5,3));
+
+console.log(myModule.CONST);
+console.log(myModule.myFunction(5,5));
